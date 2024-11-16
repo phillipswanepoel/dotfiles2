227030e227b1251499162a09ff800a96a805dbb5
phillip <phillipswanepoel@gmail.com>
2024-11-16 17:54:48 +0200

    kk

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#variables
+set -U fish_greeting
+
+# Add paths using fish_add_path
+fish_add_path -U "$HOME/.local/bin"
+set -Ux fish_user_paths $fish_user_paths ~/.local/bin
+
+# Set environment variables using set -x
+set -x FZF_DEFAULT_OPTS "--bind 'enter:become(vim {})'"
+
+# Map Home key to beginning of line
+bind \e\[1~ beginning-of-line
+bind \e\[H beginning-of-line
+
+# Map End key to end of line
+bind \e\[4~ end-of-line
+bind \e\[F end-of-line
+
